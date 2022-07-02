871f027bd1a15c3e557c15ef8e26d8a31dd487e9
vsr539444@gmail.com <vsr539444@gmail.com>
2022-07-02 18:44:06 +0530

    initial commit

---
+++ SortingSingleFor.js
@@ -0,0 +1,13 @@
+let a=[0,1,0,0,0,0,1,1,1,0,1,1,1]
+let temp;
+
+
+for(let i=0;i<a.length;i++){
+    if(a[i]<a[i+1]){
+        temp=a[i];
+        a[i]=a[i+1]
+        a[i+1]=temp
+        i=-1
+    }
+}
+console.log(a)
